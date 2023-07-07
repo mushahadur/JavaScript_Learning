de729be390f49c2c019dada4c099fba55660f8f4
Mushahadur Rahaman Khan <67184635+mushahadur@users.noreply.github.com>
2023-07-08 00:09:11 +0600

    add to commit day three

---
+++ DayTwo/Oparetors/custom.js
@@ -0,0 +1,15 @@
+let x = 10;
+x += 5;
+x;
+document.getElementById("demo").innerHTML =
+    "The total is: " + x;
+
+
+    const x1 = 0, x2 = 3, x3 = -1.5, x4 = -6.33;
+    const p1 = -x1*1;
+    const p2 = +x2*2;
+    const p3 = +x3*3;
+    const p4 = -x4*4;
+    
+    document.getElementById("demoOne").innerHTML =
+    "The total is: " +  p4;
